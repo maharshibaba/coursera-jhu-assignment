5c0efbb17e549571ca76864d17dfc371531dd516
rishav-pandey <27219302+rishav-pandey@users.noreply.github.com>
2018-02-04 22:27:08 +0530

    modified code of  assignment for module 1

--- course5/module_1/app.js
+++ course5/module_1/app.js
@@ -10,17 +10,25 @@
 		$scope.name = "";
 		$scope.messageToDisplay = "";
 		var comma = ',';
+		$scope.extra_style1 = "";
+		$scope.extra_style2 = "";
 		
 		$scope.check = function () {
 			var lengthOfString = change_string_to_array($scope.name, comma);
 			if ($scope.name == "") {
-				$scope.messageToDisplay = "Please Input Valid number of Lunch";
+				$scope.messageToDisplay = "Please Input name of Lunch first!";
+				$scope.extra_style1 = "without-data_inputArea";
+				$scope.extra_style2 = "without-data_font";
 			}
 			else if (lengthOfString <= 3) {
-				$scope.messageToDisplay = "Enjoy";
+				$scope.messageToDisplay = "Enjoy!";
+				$scope.extra_style1 = "with-data_inputArea";
+				$scope.extra_style2 = "with-data_font";
 			}
 			else if (lengthOfString > 3) {
 				$scope.messageToDisplay = "To much!";
+				$scope.extra_style1 = "with-data_inputArea";
+				$scope.extra_style2 = "with-data_font";
 			}
 		};
 
